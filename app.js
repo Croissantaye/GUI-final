adb281b54684fd255de7de2df74ca9774009327d
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2021-08-15 18:11:15 -0700

    pre servez

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const canvas = document.getElementById("webgl");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer(canvas);
+console.log(renderer.domElement);
 renderer.domElement = canvas;
 renderer.clearColor(1)
 renderer.setSize( canvas.clientWidth, canvas.clientHeight );
@@ -18,13 +19,15 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-const animate = function () {
-    requestAnimationFrame( animate );
+// const animate = function () {
+//     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
 
-    renderer.render( scene, camera );
-};
+//     renderer.render( scene, camera );
+// };
 
-animate();
+// animate();
+scene.background = new THREE.Color(1, 0, 0);
+renderer.render(scene, camera);
